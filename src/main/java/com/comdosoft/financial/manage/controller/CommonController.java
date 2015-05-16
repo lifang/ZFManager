8f51beebe424bfee0de0c9e0eeff5a606da3cb49
lonkicz <caozhen19891217@126.com>
2015-05-16 13:52:22 +0800

    百度云推送

--- src/main/java/com/comdosoft/financial/manage/controller/CommonController.java
+++ src/main/java/com/comdosoft/financial/manage/controller/CommonController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.comdosoft.financial.manage.domain.zhangfu.City;
 import com.comdosoft.financial.manage.service.CityService;
-import com.comdosoft.financial.manage.service.CustomerService;
 
 @Controller
 @RequestMapping("common")
@@ -18,18 +17,12 @@ public class CommonController {
 	
 	@Autowired
 	private CityService cityService;
-	@Autowired
-	private CustomerService customerService;
 
 	@RequestMapping(value="cities",method=RequestMethod.POST)
 	public String cities(Integer id,Model model){
 		List<City> cities = cityService.cities(id);
 		model.addAttribute("cities", cities);
 		return "common/city_option";
-	}
-	@RequestMapping(value="sendChannelId")
-	public void getChannelId(Integer customerId,String channelId){
-		customerService.updateChannelId(customerId,channelId);
 	}
 	
 }
